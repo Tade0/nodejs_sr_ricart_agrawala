fd9b286d865b42f6cbedfcdadab66305e14e799d
Tadeo <tadeusz.piskozub@gmail.com>
2014-01-30 01:38:31 +0100

    [sr] Tak szybko!

--- main.js
+++ main.js
@@ -4,19 +4,43 @@ var model = require('model.js');
 var eventManager = model.eventManager;
 
 function main() {
+  var port = 8000;
   var server = net.createServer(function(socket) {
+    eventManager.emit('addNode', socket);
 
-    addSocket(socket);
     socket.on('data', function(json) {
+      json = correctJSON(json);
       var data = JSON.parse(json);
-      model.eventManager.emit('data', {data: data});
+      model.eventManager.emit('data', {
+        data: data,
+        send: function(data) {
+          socket.write(data);
+        },
+        getSender: function() {
+          return {address: socket.remoteAddress, port: socket.remotePort};
+        }
+      });
     });
 
   });
 
+  server.on('error', function(e) {
+		if (e.code == 'EADDRINUSE') {
+			console.log('\x1b[33;1mAddress in use, retrying...\x1b[0m');
+			port++;
+			setTimeout(function () {
+				server.listen(port);
+			}, 100);
+		}
+	});
+
   server.listen(cli.portNum), function() { //'listening' listener
     console.log('Running...');
   });
+}
+
+function correctJSON(json) {
+  return json.replace(/(\w+):/,'"$1":').replace(/(\w+)\:/,'"$1":');
 }
 
 main();
--- model.js
+++ model.js
@@ -1,15 +1,90 @@
 var events = require('events');
 var clock = 0;
 var nodes = [];
+var queue = [];
+var id = Math.floor(Math.random()*7) + 1;
+var ordering = false;
+var inCritical = false;
+var criticalTime = 500;
+var criticalTimeoutId = 0;
+var okCounter = Infinity;
 
 exports.eventManager = new events.EventEmitter();
+exports.criticalTask = function() {
+  console.log('Sekcja krytyczna');
+};
 
 var eventManager = exports.eventManager;
 
-eventManager.on('data', function() {
+eventManager.on('data', function(params) {
+  if (Number(params.data.clock) > clock) {
+    clock = Number(params.data.clock);
+  }
+  switch (params.data.type) {
+    case 'order':
+      if (ordering) {
+        queue.push(params);
+      } else {
+        params.send(JSON.stringify({clock: clock, type: "ok" }));
+      }
+    break;
+    case 'ok':
+      okCounter--;
+      if (okCounter <= 0) {
+        clearTimeout(criticalTimeoutId);
+        inCritical = true;
 
+        setTimeout(exports.criticalTask, 0);
+        setTimeout(function() {
+          inCritical = false;
+          clock++;
+          eventManager.emit('releaseQueue');
+        }, criticalTime);
+      }
+    break;
+  }
 });
 
-function addNode(node) {
+eventManager.on('addNode', function(node) {
   node.push(node);
 };
+
+eventManager.on('requestCritical', function() {
+  clock++;
+  okCounter = nodes.length;
+  ordering = true;
+  nodes.forEach(function(node) {
+    node.write(JSON.stringify({clock: clock, type: "order"}));
+  });
+  criticalTimeoutId = setTimeout(function() {
+    okCounter = Infinity;
+    ordering = false;
+  }, criticalTime * (nodes.length + 1) );
+});
+
+eventManager.on('releaseQueue', function() {
+  nodes.forEach(function(node) {
+    node.write(JSON.stringify({clock: clock, type: "ok"}));
+  });
+});
+
+
+exports.compareAddresses = function(ip1, port1, ip2, port2 ) {
+  ip1n = ip1.split('.');
+  ip2n = ip2.split('.');
+  ip1n = ip1n.map(function(s) { return Number(s) });
+  ip2n = ip2n.map(function(s) { return Number(s) });
+  for (var i = 0; i < ip1.length; i++) {
+    if (ip1n[i] > ip2n[i])
+      return 1;
+    if (ip1n[i] < ip2n[i])
+      return -1;
+  }
+  if (Number(port1) > Number(port)) {
+    return 1;
+  }
+  if (Number(port1) < Number(port)) {
+    return -1;
+  }
+  return 0;
+}
